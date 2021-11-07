10366b2fe3bf567ba7f554bd6a1aec608a5f1629
andrei828 <andrei.liviu10@gmail.com>
2021-11-07 13:14:37 +0200

    fixed logging issue

--- services/Logger.js
+++ services/Logger.js
@@ -1,7 +1,6 @@
 class LoggerService {
   log(message) {
     const timestamp = new Date().toISOString();
-    this.logs.push({ message, timestamp });
     console.log(`${timestamp}: ${message}`);
   }
 }
